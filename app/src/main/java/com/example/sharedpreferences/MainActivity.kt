ca96485b34800efbdf2ecec78cc5649125ccf73a
sergeyesp <sergey_esp@i.ua>
2023-10-05 08:20:19 +0300

    Lesson2

--- app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import com.example.sharedpreferences.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val v = SharedPref.getValue(this)
+        if (v != 0) {
+            binding.tv.isVisible = false
+        }
+
+        binding.btn.setOnClickListener {
+            SharedPref.setValue(this, 1)
+        }
     }
 }
--- app/src/main/java/com/example/sharedpreferences/SharedPref.kt
+++ app/src/main/java/com/example/sharedpreferences/SharedPref.kt
@@ -1,4 +1,19 @@
 package com.example.sharedpreferences
 
+import android.content.Context
+import android.content.SharedPreferences
+
 class SharedPref {
+    companion object {
+        fun setValue(context: Context, value: Int) {
+            val sharedPref: SharedPreferences = context.getSharedPreferences("test", Context.MODE_PRIVATE)
+            val editor: SharedPreferences.Editor = sharedPref.edit()
+            editor.putInt("saveText", value)
+            editor.apply()
+        }
+        fun getValue(context: Context) : Int {
+            val sharedPref: SharedPreferences = context.getSharedPreferences("test", Context.MODE_PRIVATE)
+            return sharedPref.getInt("saveText", 0)
+        }
+    }
 }
